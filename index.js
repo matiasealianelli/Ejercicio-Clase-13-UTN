e5410ae6100ff54fea88bbcad2434070f8fb5ea9
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-05-15 19:01:05 -0300

    Resolucion de ejercicios JS

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("___DATOS PERSONALES___")
+
+//DECLARACION DE VARIABLES
+
+let nombre = "Matias";
+let edad = 24;
+let precioGym = 1500;
+
+//Mostramos valores de variables en consola:
+
+console.log(`Nombre: ${nombre}`)
+console.log(`Edad: ${edad}`)
+console.log(`Precio de gym: ${precioGym}`)
+
+/////////////////////////////////////////////////////
+
+//MODIFICACION DE VARIABLES
+
+console.log("___MODIFICACION DE VARIABLES___")
+
+//Variable edad incrementada en 1:
+
+edad += 1;
+console.log(`Edad incrementada en 1: ${edad}`)
+
+//Descuento del 10% a variable precio
+
+precioGym *= 0.9;
+console.log(`Precio con descuento: ${precioGym}`);
+
+//////////////////////////////////////////////////////
+
+//OPERACIONES ARITMETICAS.
+
+console.log("___OPERACIONES ARITMETICAS__")
+
+let cantidad1 = 15;
+let cantidad2 = 7;
+
+console.log(`Suma: ${cantidad1 + cantidad2}`)
+console.log(`Resta: ${cantidad2 - cantidad1}`)
+console.log(`Multiplicacion: ${cantidad1 * cantidad2}`)
+console.log(`Division: ${cantidad1 / cantidad2}`)
+console.log(`Obtencion de resto(Modulo): ${cantidad1 % cantidad2}`)
+/////////////////////////////////////////////////////////
